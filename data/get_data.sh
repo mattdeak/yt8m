b8198d10a2428f0e4ee3b46cad9c0d961754193f
Nash Taylor <ntaylorwss@outlook.com>
2018-06-18 16:23:08 -0700

    Added jupyter config to ignore, password-enabled jupyter in container

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 mkdir video
 cd video
+
 curl -N data.yt8m.org/download.py | tac | tac | shard=1,100 partition=2/video/train mirror=us python3
 curl -N data.yt8m.org/download.py | tac | tac | shard=1,100 partition=2/video/validate mirror=us python3
 curl -N data.yt8m.org/download.py | tac | tac | shard=1,100 partition=2/video/test mirror=us python3
