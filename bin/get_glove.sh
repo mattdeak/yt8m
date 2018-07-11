7117dc8cc335b6459f916e26d9b55ca163c82498
Nash Taylor <ntaylorwss@outlook.com>
2018-07-11 12:32:39 -0700

    Script for getting glove; move shell scripts to bin

---
+++ bin/get_glove.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ../data; cd data; # can't be sure which one so do both, one will fail
+mkdir -p glove
+if [ ! -z `ls glove/*` ]
+then
+    echo "Glove already downloaded. Exiting."
+    exit
+fi
+
+wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip glove.840B.300d.zip
+mv glove.840B.300d.txt glove.txt
+split -l 10000 glove.txt
+mv x* glove/
