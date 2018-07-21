cc23f0e821ca93a6101ea8523fd36bdefcf13d1b
Nash Taylor <ntaylorwss@outlook.com>
2018-07-21 08:53:15 -0700

    Add adjacency matrix and change from object to functions

--- lib/labels.py
+++ lib/labels.py
@@ -2,13 +2,13 @@ import os
 import time
 import string
 import itertools
-import multiprocessing
+import functools
 import numpy as np
 import pandas as pd
-from .util import timed, imputer_dict
+import multiprocessing
+from .util import imputer_dict, timed
 
-
-def file_reader(num):
+def file_reader(name, num):
     """Kernel for GLoVE parallel loading. Parses a single file for its contents.
 
     Parameters
@@ -26,7 +26,7 @@ def file_reader(num):
     elif num >= 90:
         num = 9000 + num - 90
     out = []
-    with open(f'/home/data/glove/x{num}', 'rb') as f:
+    with open(f'/home/data/{name}/x{num}', 'rb') as f:
         for line in f:
             splitLine = line.split()
             word = splitLine[0].decode()
@@ -35,156 +35,62 @@ def file_reader(num):
     return out
 
 
-class LabelHandler:
-    """Responsible for loading labels and converting them to any other format, e.g. glove vectors.
+@timed
+def load_embedding(name='glove', default_text='the'):
+    """Loads dictionary mapping individual words to their GLoVE vectors."""
+    start_time = time.time()
+    p = multiprocessing.Pool(multiprocessing.cpu_count() // 2)
+    results = p.map(functools.partial(file_reader, 'glove'),
+                    range(len(os.listdir(f'/home/data/{name}'))),
+                    )
+    embedding = [pair for result in results for pair in result]
+    embedding = imputer_dict(default_text, embedding)
+    return embedding
 
-    New formats for labels should be added as methods to this class.
+def load_text_labels():
+    return dict(pd.read_csv('/home/data/label_names_2018.csv').values)
 
-    Parameters
-    ----------
-    subset : {'train', 'validate', 'test'}
-        Name of dataset to load labels for.
-    default_text_label : str
-        In the event that a label int is not associated with a str, default to this.
-    default_glove_word : str
-        In the event that the GLoVE embedding does not include a word, default to this.
+def load_int_labels(subset, limit):
+    filename = f'/home/data/labels/{subset}_labels.txt'
+    out = {}
+    with open(filename, 'r') as file:
+        for i,line in enumerate(file):
+            if limit >= 0 and i >= limit:
+                break
+            data = line.split(' ')
+            out[data[0]] = [int(datum) for datum in data[1:]]
+    return out
 
-    Attributes
-    ----------
-    punc_remover : dict
-        Utility for removing punctuation from labels to pass them to GLoVE.
-    default_text_label : str
-        In the event that a label int is not associated with a str, default to this.
-    default_glove_word : str
-        In the event that the GLoVE embedding does not include a word, default to this.
-    video_to_int : dict of {str: list of int}
-        Maps video ID to its integer labels.
-    int_to_name : dict of {int: str}
-        Maps integer label to text label, e.g. "video game".
-    word_to_glove : dict of {str: np.array}
-        Maps singular word from label, e.g. "video", to its 300-D GLoVE vector.
-    """
-    def __init__(self, subset, limit=-1,
-                 default_text_label='undefined',
-                 default_glove_word='the'):
-        self.punc_remover = str.maketrans('', '', string.punctuation)
-        self.filename = f'/home/data/labels/{subset}_labels.txt'
-        self.limit = limit
-        self.default_text_label = default_text_label
-        self.default_glove_word = default_glove_word
-        self.video_to_int = None
-        self.int_to_name = None
-        self.word_to_glove = None
 
-    @timed
-    def _load_labels(self, filename):
-        """Loads labels from file as dictionary.
+def text_to_embedding(text, embedding_dict):
+    punc_remover = str.maketrans('', '', string.punctuation)
+    words = text.replace('-', ' ')
+    words = words.split(' ')
+    words = [word.lower().translate(punc_remover) for word in words]
+    if len(words) > 1:
+        out = np.array([embedding_dict[word] for word in words]).mean(axis=0)
+    else:
+        out = embedding_dict[words[0]]
+    return out
 
-        Parameters
-        ----------
-        filename : str
-            Full or relative filepath of the file to be loaded from.
 
-        Returns
-        -------
-        dict of {str: list of int}
-            Mapping of video ID to list of integer labels.
-        """
-        out = {}
-        with open(filename, 'r') as file:
-            for i,line in enumerate(file):
-                if self.limit >= 0 and i >= self.limit:
-                    break
-                data = line.split(' ')
-                out[data[0]] = [int(datum) for datum in data[1:]]
-        return out
+def embedding_matrix(subset, limit, embedding):
+    int_to_text = load_text_labels()
+    X = load_int_labels(subset, limit)
+    X = sorted(list(set(val for obs in X.values() for val in obs)))
+    X = list(map(lambda i: int_to_text[i], X))
+    X = [x if isinstance(x, str) else 'the' for x in X]
+    X = np.array(list(map(lambda x: text_to_embedding(x, embedding), X)))
+    return X
 
-    @timed
-    def _load_names(self):
-        """Loads dictionary mapping label integers to text names."""
-        return dict(pd.read_csv('/home/data/label_names_2018.csv').values)
 
-    @timed
-    def _load_glove(self):
-        """Loads dictionary mapping individual words to their GLoVE vectors."""
-        start_time = time.time()
-        p = multiprocessing.Pool(multiprocessing.cpu_count()-2)
-        results = p.map(file_reader, range(len(os.listdir('/home/data/glove'))))
-        glove = [pair for result in results for pair in result]
-        if self.default_text_label:
-            glove = imputer_dict(self.default_glove_word, glove)
-        else:
-            glove = dict(glove)
-        return glove
-
-    def get(self):
-        if not self.video_to_int:
-            self.video_to_int = self._load_labels(self.filename)
-        return self.video_to_int
-
-    def get_text(self):
-        """Converts loaded integer labels to their text names."""
-        if not self.video_to_int:
-            self.video_to_int = self._load_labels(self.filename)
-        if not self.int_to_name:
-            self.int_to_name = self._load_names()
-
-        text_labels = {}
-        for video, labels in self.video_to_int.items():
-            try:
-                video_labels = [self.int_to_name[label] for label in labels]
-                video_labels = [label if isinstance(label, str) else self.default_text_label
-                                for label in video_labels]
-                text_labels[video] = video_labels
-            except KeyError:
-                continue
-        return text_labels
-
-    def get_glove(self):
-        """Converts loaded integer labels to their GLoVE vectors."""
-        if not self.video_to_int:
-            self.video_to_int = self._load_labels(self.filename)
-        if not self.word_to_glove:
-            self.word_to_glove = self._load_glove()
-        text_labels = self.to_text()
-        punc_remover = str.maketrans('', '', string.punctuation)
-        text_to_glove = {}
-        for key, text_group in text_labels.items():
-            for text in text_group:
-                if text in text_to_glove:
-                    continue
-                text = text.replace('-', ' ') # e.g. "action-adventure game" is three words
-                words = text.split(' ')
-                words = [word.lower().translate(punc_remover) for word in words]
-                if len(words) > 1:
-                    text_to_glove[text] = np.array([self.word_to_glove[word]
-                                                      for word in words]
-                                                     ).mean(axis=0)
-                else:
-                    text_to_glove[text] = self.word_to_glove[words[0]]
-        return text_to_glove
-
-    def get_graph(self, identity=False):
-        """Get adjacency matrix for graph of labels, where edges are common observations.
-
-        Parameters
-        ----------
-        identity : bool
-            Whether self-loops are included in the matrix; i.e. the addition of the identity.
-
-        Returns
-        -------
-        adj_matrix : np.array
-            A square 2D Numpy array that represents the adjacency matrix of the present labels.
-        """
-        if not self.video_to_int:
-            self.video_to_int = self._load_labels(self.filename)
-
-        uniques = list(set(val for obs in self.video_to_int.values() for val in obs))
-        adj_matrix = np.zeros([len(uniques)]*2)
-        for obs in self.video_to_int.values():
-            for label1, label2 in itertools.product(obs, obs):
-                if label1 != label2:
-                    coords = (uniques.index(label1), uniques.index(label2))
-                    adj_matrix[coords] = 1
-        return adj_matrix
+def adjacency_matrix(subset, limit):
+    lists = load_int_labels(subset, limit)
+    uniques = sorted(list(set(val for obs in lists for val in obs)))
+    adj_matrix = np.zeros([len(uniques)]*2)
+    for obs in lists:
+        for label1, label2 in itertools.product(obs, obs):
+            if label1 != label2:
+                coords = (uniques.index(label1), uniques.index(label2))
+                adj_matrix[coords] = 1
+    return adj_matrix
